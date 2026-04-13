f6f1a7cc5d5c806e1c320db5705a3f8913b7d52c
Hlamalani <hlami0524@gmail.com>
2026-04-13 04:47:55 +0200

    Add backend files

---
+++ server.js
@@ -0,0 +1,102 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+
+const PORT = process.env.PORT || 3000;
+
+let newsCache = null;
+let lastFetchTime = 0;
+const CACHE_DURATION = 1000 * 60 * 10;
+
+app.get("/", (req, res) => {
+  res.send("API is running 🚀");
+});
+
+app.get("/api/news", async (req, res) => {
+  try {
+    const now = Date.now();
+
+    if (newsCache && (now - lastFetchTime < CACHE_DURATION)) {
+      console.log("Serving from cache 🧠");
+      return res.json(newsCache);
+    }
+
+    console.log("Fetching from GNews 🌍");
+
+    const response = await fetch(
+      "https://gnews.io/api/v4/top-headlines?country=za&apikey=86694ebbca323058620c4731f041f8b6"
+    );
+
+    const data = await response.json();
+
+    newsCache = data;
+    lastFetchTime = now;
+
+    res.json(data);
+
+  } catch (err) {
+    res.status(500).json({ error: "Failed to fetch news" });
+  }
+}); 
+
+
+app.get("/api/sports", async (req, res) => {
+  try {
+    const now = Date.now();
+
+    if (sportsCache && (now - lastFetchTime < CACHE_DURATION)) {
+      console.log("Serving from cache 🧠");
+      return res.json(sportsCache);
+    }
+
+    console.log("Fetching from GNews 🌍");
+
+    const response = await fetch(
+      "https://gnews.io/api/v4/top-headlines?country=za&apikey=86694ebbca323058620c4731f041f8b6"
+    );
+
+    const data = await response.json();
+
+    politicsCache = data;
+    lastFetchTime = now;
+
+    res.json(data);
+
+  } catch (err) {
+    res.status(500).json({ error: "Failed to fetch news" });
+  }
+});
+
+
+app.get("/api/politics", async (req, res) => {
+  try {
+    const now = Date.now();
+
+    if (politicsCache && (now - lastFetchTime < CACHE_DURATION)) {
+      console.log("Serving from cache 🧠");
+      return res.json(politicsCache);
+    }
+
+    console.log("Fetching from GNews 🌍");
+
+    const response = await fetch(
+      "https://gnews.io/api/v4/top-headlines?country=za&apikey=86694ebbca323058620c4731f041f8b6"
+    );
+
+    const data = await response.json();
+
+    newsCache = data;
+    lastFetchTime = now;
+
+    res.json(data);
+
+  } catch (err) {
+    res.status(500).json({ error: "Failed to fetch news" });
+  }
+});
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on network`);
+});
